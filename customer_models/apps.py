eb6f459e06749da17b1d80ca6151b96d56513187
reimibeta <pinsoneatra.soneatra846@gmail.com>
2021-06-02 09:37:35 +0700

    .

--- customer_models/apps.py
+++ customer_models/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 
 
 class CustomerConfig(AppConfig):
-    # default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'customer_models'
     verbose_name = 'Customers'
